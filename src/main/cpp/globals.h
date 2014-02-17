a2e17adc41488a1f3a79b315d14e9429ba35e91c
Richard Warburton <richard.warburton@gmail.com>
2014-02-17 21:08:47 +0000

    kill no longer used constant

--- src/main/cpp/globals.h
+++ src/main/cpp/globals.h
@@ -169,9 +169,6 @@ private:
 // Number of times per second that we profile
 static const int kNumInterrupts = 100;
 
-// Maximum number of stack traces
-static const int kMaxStackTraces = 3000;
-
 // Maximum number of frames to store from the stack traces sampled.
 static const int kMaxFramesToCapture = 128;
 
