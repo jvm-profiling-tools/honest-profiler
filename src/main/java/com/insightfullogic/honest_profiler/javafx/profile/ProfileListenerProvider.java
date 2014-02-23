2508af5b44609c642348db9e2d0b2e6b91596de6
Richard Warburton <richard.warburton@gmail.com>
2014-02-23 09:27:02 +0000

    refactor to remove util class

--- src/main/java/com/insightfullogic/honest_profiler/javafx/profile/ProfileListenerProvider.java
+++ src/main/java/com/insightfullogic/honest_profiler/javafx/profile/ProfileListenerProvider.java
@@ -1,18 +1,15 @@
 package com.insightfullogic.honest_profiler.javafx.profile;
 
-import com.insightfullogic.honest_profiler.collector.Profile;
 import com.insightfullogic.honest_profiler.collector.ProfileListener;
-import com.insightfullogic.honest_profiler.util.Listeners;
 import org.picocontainer.injectors.ProviderAdapter;
 
 public class ProfileListenerProvider extends ProviderAdapter {
 
     public ProfileListener provide(FlatViewModel flatModel, TreeViewModel treeModel) {
-        Listeners<Profile> listener = new Listeners<Profile>()
-                .of(flatModel::accept)
-                .of(treeModel::accept);
-
-        return listener::accept;
+        return profile -> {
+            flatModel.accept(profile);
+            treeModel.accept(profile);
+        };
     }
 
 }
