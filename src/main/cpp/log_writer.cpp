ba0361330e8289031ba0397057b4fc513db00eb3
nitsanw <nitsanw@yahoo.com>
2015-10-07 13:25:02 +0200

    Revised bci2line algo. Suffiently differing from HPROF to remove doubt.

--- src/main/cpp/log_writer.cpp
+++ src/main/cpp/log_writer.cpp
@@ -26,71 +26,64 @@ void LogWriter::writeValue(const T &value) {
 static int64_t getThreadId(JNIEnv *env_id) {
     return (int64_t) env_id;
 }
-/** following the same algorithm as HPROF */
-static jint map_loc2line(jint bci, jvmtiLineNumberEntry *table, jint count) {
-    jint line_number;
-    int i;
-    int start;
-    int half;
 
-    line_number = -101;
-    if ( count == 0 ) {
-        return line_number;
-    }
-
-    /* Do a binary search */
-    half = count >> 1;
-    start = 0;
+static jint bci2line(jint bci, jvmtiLineNumberEntry *table, jint entry_count) {
+	jint line_number = -101;
+	if ( entry_count == 0 ) {
+		return line_number;
+	}
+	line_number = -102;
+    // We're looking for a line whose 'start_location' is nearest AND >= BCI
+	// We assume the table is sorted by 'start_location'
+    // Do a binary search to quickly approximate 'start_index" in table
+	int half = entry_count >> 1;
+    int start_index = 0;
     while ( half > 0 ) {
-        jint start_location;
-
-        start_location = table[start + half].start_location;
+        jint start_location = table[start_index + half].start_location;
         if ( bci > start_location ) {
-            start = start + half;
+            start_index = start_index + half;
         } else if ( bci == start_location ) {
-            start = start + half;
-            break;
+        	// gotcha
+            return table[start_index + half].line_number;
         }
         half = half >> 1;
     }
 
 
-    /* Now start the table search */
-    for ( i = start ; i < count ; i++ ) {
+    /* Now start the table search from approximated start_index */
+    for (int i = start_index ; i < entry_count ; i++ ) {
+    	// start_location > BCI: means line starts after the BCI, we'll take the previous match
         if ( bci < table[i].start_location ) {
             break;
+        }
+        else if (bci == table[i].start_location) {
+        	// gotcha
+        	return table[i].line_number;
         }
         line_number = table[i].line_number;
     }
     return line_number;
 }
 
-static void jvmtiDeallocate(jvmtiEnv *jvmti, void *ptr)
-{
-    if ( ptr != NULL ) {
-        jvmtiError error;
-
-        error = jvmti->Deallocate ((unsigned char*)ptr);
-        if ( error != JVMTI_ERROR_NONE ) {
-            // TODO: HPROF_JVMTI_ERROR(error, "Cannot deallocate jvmti memory");
-        }
-    }
-}
 jint LogWriter::getLineNo(jint bci, jmethodID methodId) {
 	if(bci <= 0) {
 		return bci;
 	}
-	// TODO: cache line number tables?
-	// Caching is risky as the growth is not capped. An alternative is to serialize the line table once per method
-	// but might bloat log? current approach is brute..
+
 	jvmtiLineNumberEntry* jvmti_table;
 	jint entry_count;
 	jvmtiError err = jvmti_->GetLineNumberTable(methodId, &entry_count, &jvmti_table);
 	if (err != JVMTI_ERROR_NONE) {
 		return -100;
 	}
-	jint lineno = map_loc2line(bci, jvmti_table, entry_count);
-	jvmtiDeallocate(jvmti_, jvmti_table);
+
+	jint lineno = bci2line(bci, jvmti_table, entry_count);
+
+	// cleanup
+	err = jvmti_->Deallocate ((unsigned char*)jvmti_table);
+	if ( err != JVMTI_ERROR_NONE ) {
+		// TODO: Log err?
+	}
 	return lineno;
 }
 
