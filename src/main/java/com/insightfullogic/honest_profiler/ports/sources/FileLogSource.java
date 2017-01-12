4f16eff4a9627343298c7431ec28b9fca790c420
PhRX <philip.arickx@gmail.com>
2017-01-12 09:48:38 +0100

    Fix for #175 : Implemented "moving window" buffer
    FileLogSource now has a fixed-size buffer which "moves along the file"
    as data is read.

--- src/main/java/com/insightfullogic/honest_profiler/ports/sources/FileLogSource.java
+++ src/main/java/com/insightfullogic/honest_profiler/ports/sources/FileLogSource.java
@@ -21,8 +21,7 @@
  **/
 package com.insightfullogic.honest_profiler.ports.sources;
 
-import com.insightfullogic.honest_profiler.core.sources.CantReadFromSourceException;
-import com.insightfullogic.honest_profiler.core.sources.LogSource;
+import static java.nio.channels.FileChannel.MapMode.READ_ONLY;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,23 +30,50 @@ import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
-import static java.nio.channels.FileChannel.MapMode.READ_ONLY;
+import com.insightfullogic.honest_profiler.core.sources.CantReadFromSourceException;
+import com.insightfullogic.honest_profiler.core.sources.LogSource;
 
+/**
+ * LogSource implementation which maintains a fixed-size memory-mapped window
+ * ({@value #BUFFER_SIZE} bytes) on the file. The read() method checks whether a
+ * certain amount of the current buffer ({@value #ELASTICITY} bytes) has been
+ * read already, and if so, remaps the buffer to the new position.
+ *
+ * The ELASTICITY is provided for the benefit of the LogParser + Conductor
+ * logic. If a partial record is read, the Conductor will sleep for a bit. So
+ * ideally, BUFFER_SIZE - ELASTICITY should be large enough so that an "entire
+ * record" (i.e. an entire stack) which might start at the last byte within the
+ * ELASTICITY portion still would fit into the remains of the buffer.
+ */
 public class FileLogSource implements LogSource
 {
+    // Class Properties
 
+    // Fixed buffer size
+    private static final int BUFFER_SIZE = 1024 * 1024 * 5;
+    // Remap if more than ELASTICITY has been read from the current buffer
+    private static final int ELASTICITY = 1024 * 1024 * 4;
+
+    // Instance Properties
+
+    private final RandomAccessFile raf;
     private final FileChannel channel;
     private final File file;
 
     private MappedByteBuffer buffer;
+    // the offset in the file where the current buffer starts.
+    private long currentOffset = 0;
+
+    // Instance Constructors
 
     public FileLogSource(final File file)
     {
         this.file = file;
         try
         {
-            channel = new RandomAccessFile(file, "r").getChannel();
-            remapFile(channel.size());
+            raf = new RandomAccessFile(file, "r");
+            channel = raf.getChannel();
+            mapBuffer(0);
         }
         catch (IOException e)
         {
@@ -55,26 +81,27 @@ public class FileLogSource implements LogSource
         }
     }
 
-    // Shame there's no simple abstraction for reading over both files
-    // and network bytebuffers
-    private void remapFile(final long size) throws IOException
-    {
-        buffer = channel.map(READ_ONLY, 0, size);
+    // Instance Accessors
 
+    public File getFile()
+    {
+        return file;
     }
+
+    // LogSource Implementation
 
     @Override
     public ByteBuffer read()
     {
         try
         {
-            int limit = buffer.limit();
-            long channelSize = channel.size();
-            if (channelSize > limit)
+            // If we've read more than ELASTICITY bytes, the
+            // currentOffset is updated and the buffer is remapped.
+            int position = buffer.position();
+            if (position > ELASTICITY)
             {
-                int oldPosition = buffer.position();
-                remapFile(channelSize);
-                buffer.position(oldPosition);
+                currentOffset += position;
+                mapBuffer(currentOffset);
             }
         }
         catch (IOException e)
@@ -85,22 +112,31 @@ public class FileLogSource implements LogSource
         return buffer;
     }
 
-    public File getFile()
-    {
-        return file;
-    }
-
     @Override
     public void close() throws IOException
     {
-        channel.close();
+        buffer = null;
+        raf.close();
+    }
+
+    // Shame there's no simple abstraction for reading over both files and
+    // network bytebuffers
+
+    /**
+     * Replaces the current buffer by a new ByteBuffer which is memory-mapped
+     * onto a BUFFER_SIZE (10 MB at time of writing) window starting at the
+     * specified offset.
+     *
+     * @throws IOException
+     */
+    private void mapBuffer(long offset) throws IOException
+    {
+        buffer = channel.map(READ_ONLY, offset, BUFFER_SIZE);
     }
 
     @Override
     public String toString()
     {
-        return "FileLogSource{" +
-            "file=" + file +
-            '}';
+        return "FileLogSource{" + "file=" + file + '}';
     }
 }
