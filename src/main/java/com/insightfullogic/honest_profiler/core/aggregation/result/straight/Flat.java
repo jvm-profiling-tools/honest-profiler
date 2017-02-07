c65ff56765c24574b3b58850d35e3da9be4feb3c
PhRX <philip.arickx@gmail.com>
2017-02-07 17:22:48 +0100

    More documentation and unused method removal

--- src/main/java/com/insightfullogic/honest_profiler/core/aggregation/result/straight/Flat.java
+++ src/main/java/com/insightfullogic/honest_profiler/core/aggregation/result/straight/Flat.java
@@ -9,14 +9,31 @@ import com.insightfullogic.honest_profiler.core.aggregation.AggregationProfile;
 import com.insightfullogic.honest_profiler.core.aggregation.filter.FilterSpecification;
 import com.insightfullogic.honest_profiler.core.aggregation.grouping.CombinedGrouping;
 import com.insightfullogic.honest_profiler.core.aggregation.result.Aggregation;
+import com.insightfullogic.honest_profiler.core.profiles.lean.LeanNode;
 
+/**
+ * A Flat is a list-based {@link Aggregation}. {@link LeanNode}s are aggregated by using the {@link CombinedGrouping} to
+ * extract a String key, and each {@link Entry} in the Flat is the aggregation of all {@link LeanNode}s with the same
+ * key.
+ */
 public class Flat extends Aggregation<Entry>
 {
+    // Instance Constructors
+
+    /**
+     * Create an empty {@link Flat} for the specified {@link AggregationProfile} and {@link CombinedGrouping}.
+     *
+     * @param source the {@link AggregationProfile} whose {@link LeanNode}s are aggregated into this Flat
+     * @param grouping the {@link CombinedGrouping} used for aggregation
+     */
     public Flat(AggregationProfile source, CombinedGrouping grouping)
     {
         super(source, grouping, new ArrayList<>());
     }
 
+    /**
+     * @see Aggregation#Aggregation(AggregationProfile, CombinedGrouping, List)
+     */
     public Flat(AggregationProfile source, CombinedGrouping grouping, List<Entry> data)
     {
         super(source, grouping, data);
--- src/main/java/com/insightfullogic/honest_profiler/core/aggregation/result/straight/Node.java
+++ src/main/java/com/insightfullogic/honest_profiler/core/aggregation/result/straight/Node.java
@@ -22,14 +22,28 @@ import com.insightfullogic.honest_profiler.core.profiles.lean.LeanNode;
  */
 public class Node extends Entry
 {
+    // Instance Properties
+
     private Map<String, Node> children;
 
+    // Instance Constructors
+
+    /**
+     * Create an empty Node for the specified {@link Aggregation}.
+     *
+     * @param aggregation the {@link Aggregation} the created Node belongs to
+     */
     public <T extends Keyed<String>> Node(Aggregation<T> aggregation)
     {
         super(aggregation);
         this.children = new HashMap<>();
     }
 
+    /**
+     * Create a Node based on an {@link Entry}. The information of the {@link Entry} is copied into this Node.
+     *
+     * @param entry the {@link Entry} the Node is based on
+     */
     public Node(Entry entry)
     {
         this(entry.getAggregation());
@@ -39,22 +53,31 @@ public class Node extends Entry
     /**
      * Copy Constructor.
      *
-     * @param entry
-     * @param children
+     * @param node the Node being copied
+     * @param children the (new) children of the Node
      */
-    public Node(Node entry, List<Node> children)
+    private Node(Node node, List<Node> children)
     {
-        this(entry.getAggregation());
-        entry.copyInto(this);
+        this(node.getAggregation());
+        node.copyInto(this);
         children.forEach(child -> this.children.put(child.getKey(), child));
     }
 
+    /**
+     * Returns the children of the Node.
+     *
+     * @return the children of the Node
+     */
     public List<Node> getChildren()
     {
         return new ArrayList<>(children.values());
     }
 
-    // Calculate deepest stack depth in descendants. Return 0 if there are no children.
+    /**
+     * Calculate the depth of the (sub)tree with this Node as root. Returns 0 if there are no children.
+     *
+     * @return the depth of the (sub)tree with this Node as root, whereby an empty tree has depth 0
+     */
     public int getDescendantDepth()
     {
         if (children.isEmpty())
@@ -70,24 +93,25 @@ public class Node extends Entry
         return depth;
     }
 
-    public void addAll(Map<String, Node> newChildren)
-    {
-        newChildren.values().forEach(
-            newChild -> this.children
-                .compute(newChild.getKey(), (k, v) -> v == null ? newChild : v.combine(newChild)));
-    }
-
-    public void add(String key, LeanNode node)
-    {
-        super.setKey(key);
-        super.add(node);
-    }
-
+    /**
+     * Adggregates a Node into the children of this Node.
+     *
+     * @param child the Node to be aggregated as child
+     * @return the Node resulting from the aggregation
+     */
     public Node addChild(Node child)
     {
         return children.compute(child.getKey(), (k, v) -> v == null ? child : v.combine(child));
     }
 
+    /**
+     * Aggregates a {@link LeanNode} into the children of this Node, using the specified {@link CombinedGrouping} to
+     * determine the aggregation key, and recursively aggregating the {@link LeanNode} descendants as well if specified.
+     *
+     * @param child the {@link LeanNode} to be aggregated into the children of this Node
+     * @param grouping the {@link CombinedGrouping} used for determining the aggregation key
+     * @param recurse a boolean specifying whether the {@link LeanNode} descendants should be aggregated recursively
+     */
     public void addChild(LeanNode child, CombinedGrouping grouping, boolean recurse)
     {
         // Construct intermediate Node
@@ -105,6 +129,12 @@ public class Node extends Entry
         }
     }
 
+    /**
+     * Combines another Node into this one. The descendants will also be combined recursively.
+     *
+     * @param other he Node to be combined into this Node
+     * @return this Node
+     */
     public Node combine(Node other)
     {
         super.combine(other);
@@ -114,6 +144,11 @@ public class Node extends Entry
         return this;
     }
 
+    /**
+     * Returns a copy of this Node.
+     *
+     * @return a copy of this Node
+     */
     public Node copy()
     {
         List<Node> newChildren = children.values().stream().map(child -> child.copy())
@@ -121,6 +156,13 @@ public class Node extends Entry
         return new Node(this, newChildren);
     }
 
+    /**
+     * Returns a copy of this Node applying a filter to itself and any descendants. The method returns <null> if no
+     * children are accepted by the filter and the node itself isn't accepted either.
+     *
+     * @param filter a {@link Predicate} for accepting Nodes
+     * @return the filtered Node or <null> if the Node and none of its descendants are accepted
+     */
     public Node copyWithFilter(Predicate<Node> filter)
     {
         List<Node> newChildren = children.values().stream()
@@ -129,11 +171,21 @@ public class Node extends Entry
         return newChildren.size() > 0 || filter.test(this) ? new Node(this, newChildren) : null;
     }
 
+    /**
+     * Return a {@link Stream} of Nodes consisting of this Node and all its descendants.
+     *
+     * @return a {@link Stream} of Nodes consisting of this Node and all its descendants
+     */
     public Stream<Node> flatten()
     {
         return concat(of(this), children.values().stream().flatMap(Node::flatten));
     }
 
+    /**
+     * Return a {@link Stream} of Nodes consisting of all the descendants of this Node. This Node is not included.
+     *
+     * @return a {@link Stream} of Nodes consisting of all the descendants of this Node
+     */
     public Stream<Node> flattenDescendants()
     {
         return children.values().stream().flatMap(Node::flatten);
