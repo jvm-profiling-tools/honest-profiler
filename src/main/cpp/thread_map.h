0d9cdc1c2c1e55b450073512a63aa1879f1d4a5b
Ivan Kavaliou <ivanich3d@gmail.com>
2017-10-18 21:10:37 +0100

    agent: add concurrent thread map test and format it

--- src/main/cpp/thread_map.h
+++ src/main/cpp/thread_map.h
@@ -1,163 +1,155 @@
 #ifndef THREAD_MAP_H
 #define THREAD_MAP_H
 
-#include <jvmti.h>
-#include <jni.h>
-#include <string.h>
 #include "concurrent_map.h"
-
+#include <jni.h>
+#include <jvmti.h>
+#include <string.h>
 
 int gettid();
 
-template <typename PType>
-struct PointerHasher {
-	/* Numerical Recipes, 3rd Edition */
-	static int64_t hash(void *p) {
-		int64_t v = (int64_t)p / sizeof(PType);
-		v = v * 3935559000370003845 + 2691343689449507681;
+template <typename PType> struct PointerHasher {
+  /* Numerical Recipes, 3rd Edition */
+  static int64_t hash(void *p) {
+    int64_t v = (int64_t)p / sizeof(PType);
+    v = v * 3935559000370003845 + 2691343689449507681;
 
-  		v ^= v >> 21;
-  		v ^= v << 37;
-  		v ^= v >>  4;
+    v ^= v >> 21;
+    v ^= v << 37;
+    v ^= v >> 4;
 
-  		v *= 4768777513237032717;
+    v *= 4768777513237032717;
 
-  		v ^= v << 20;
-  		v ^= v >> 41;
-  		v ^= v <<  5;
+    v ^= v << 20;
+    v ^= v >> 41;
+    v ^= v << 5;
 
-  		return v;
-	}
+    return v;
+  }
 };
 
 const int kInitialMapSize = 256;
 
 class GCHelper {
 public:
-	static map::GC::EpochType attach() {
-		return map::DefaultGC.attachThread();
-	}
+  static map::GC::EpochType attach() { return map::DefaultGC.attachThread(); }
 
-	static void detach(map::GC::EpochType &localEpoch) {
-		if (localEpoch != map::GC::kEpochInitial)
-			map::DefaultGC.detachThread(localEpoch);
-	}
+  static void detach(map::GC::EpochType &localEpoch) {
+    if (localEpoch != map::GC::kEpochInitial)
+      map::DefaultGC.detachThread(localEpoch);
+  }
 
-	static void safepoint(map::GC::EpochType &localEpoch) {
-		map::DefaultGC.safepoint(localEpoch);
-	}
+  static void safepoint(map::GC::EpochType &localEpoch) {
+    map::DefaultGC.safepoint(localEpoch);
+  }
 
-	static void signalSafepoint(map::GC::EpochType &localEpoch) {
-		map::DefaultGC.ss_safepoint(localEpoch);
-	}
+  static void signalSafepoint(map::GC::EpochType &localEpoch) {
+    map::DefaultGC.ss_safepoint(localEpoch);
+  }
 };
 
 struct ThreadBucket {
-	const int tid;
-	std::string name;
-	std::atomic_int refs;
-	map::GC::EpochType localEpoch;
+  const int tid;
+  std::string name;
+  std::atomic_int refs;
+  map::GC::EpochType localEpoch;
 
-	explicit ThreadBucket(int id, const char *n) : tid(id), name(n), refs(1), localEpoch(GCHelper::attach()) {
-	}
+  explicit ThreadBucket(int id, const char *n)
+      : tid(id), name(n), refs(1), localEpoch(GCHelper::attach()) {}
 
-	int release() {
-		return refs.fetch_sub(1, std::memory_order_acquire);
-	}
+  int release() { return refs.fetch_sub(1, std::memory_order_acquire); }
 
-	~ThreadBucket() {}
+  ~ThreadBucket() {}
 };
 
-// ThreadBucket* wrapper that does atomic reference counting and only supports move semantic.
-// Here weak means that ref counter is not incremented when wrapper is created, but it will 
-// be decremented once object is destroyed.
+/* ThreadBucket* wrapper that does atomic reference counting and only supports
+   move semantic. Here weak means that ref counter is not incremented when
+   wrapper is created, but it will be decremented once object is destroyed.
+*/
 class ThreadBucketPtr {
 public:
-	explicit ThreadBucketPtr(ThreadBucket *b, bool weak = true) : bucket(b) {
-		if (bucket && !weak) {
-			int prev = bucket->refs.fetch_add(1, std::memory_order_relaxed);
-			assert(prev >= 0);
-			if (prev == 0) {
-				// return to released state
-				bucket->refs.fetch_sub(1, std::memory_order_relaxed);
-			}
-		}
-	}
+  explicit ThreadBucketPtr(ThreadBucket *b, bool weak = true) : bucket(b) {
+    if (bucket && !weak) {
+      int prev = bucket->refs.fetch_add(1, std::memory_order_relaxed);
+      assert(prev >= 0);
+      if (prev == 0) {
+        // return to released state
+        bucket->refs.fetch_sub(1, std::memory_order_relaxed);
+      }
+    }
+  }
 
-	ThreadBucketPtr(ThreadBucketPtr&& tb) : bucket(tb.bucket) {
-		tb.bucket = nullptr;
-	}
+  ThreadBucketPtr(ThreadBucketPtr &&tb) : bucket(tb.bucket) {
+    tb.bucket = nullptr;
+  }
 
-	ThreadBucketPtr(const ThreadBucketPtr& tb) = delete;
+  ThreadBucketPtr(const ThreadBucketPtr &tb) = delete;
 
-	ThreadBucketPtr& operator=(ThreadBucketPtr&& tb) {
-		if (bucket && bucket->release() == 1) {
-			delete bucket;
-		}
-		bucket = tb.bucket;
-		tb.bucket = nullptr;
-		return *this;
-	}
+  ThreadBucketPtr &operator=(ThreadBucketPtr &&tb) {
+    if (bucket && bucket->release() == 1) {
+      delete bucket;
+    }
+    bucket = tb.bucket;
+    tb.bucket = nullptr;
+    return *this;
+  }
 
-	ThreadBucketPtr& operator=(const ThreadBucketPtr& tb) = delete;
+  ThreadBucketPtr &operator=(const ThreadBucketPtr &tb) = delete;
 
-	ThreadBucket* operator->() {
-		return bucket;
-	}
+  ThreadBucket *operator->() { return bucket; }
 
-	bool defined() const {
-		return bucket != nullptr;
-	}
+  bool defined() const { return bucket != nullptr; }
 
-	void reset() {
-		if (bucket && bucket->release() == 1) {
-			delete bucket;
-		}
-		bucket = nullptr;
-	}
+  void reset() {
+    if (bucket && bucket->release() == 1) {
+      delete bucket;
+    }
+    bucket = nullptr;
+  }
 
-	~ThreadBucketPtr() {
-		if (bucket && bucket->release() == 1) {
-			delete bucket;
-		}
-	}
+  ~ThreadBucketPtr() {
+    if (bucket && bucket->release() == 1) {
+      delete bucket;
+    }
+  }
 
 private:
-	ThreadBucket *bucket;
+  ThreadBucket *bucket;
 };
 
-template <typename MapProvider>
-class ThreadMapBase {
+template <typename MapProvider> class ThreadMapBase {
 private:
-	MapProvider map;
+  MapProvider map;
 
 public:
-	explicit ThreadMapBase(int capacity = kInitialMapSize) : map(capacity) {}
+  explicit ThreadMapBase(int capacity = kInitialMapSize) : map(capacity) {}
 
-	void put(JNIEnv *jni_env, const char *name) {
-		put(jni_env, name, gettid());
-	}
+  void put(JNIEnv *jni_env, const char *name) { put(jni_env, name, gettid()); }
 
-	void put(JNIEnv *jni_env, const char *name, int tid) {
-		ThreadBucket *info = new ThreadBucket(tid, name);
-		ThreadBucketPtr oldRef((ThreadBucket*)map.put((map::KeyType)jni_env, (map::ValueType)info)); // weak ref to object
-		GCHelper::safepoint(info->localEpoch); // each thread inserts once
-	}
+  void put(JNIEnv *jni_env, const char *name, int tid) {
+    ThreadBucket *info = new ThreadBucket(tid, name);
+    ThreadBucketPtr oldRef((ThreadBucket *)map.put(
+        (map::KeyType)jni_env, (map::ValueType)info)); // weak ref to object
+    GCHelper::safepoint(info->localEpoch); // each thread inserts once
+  }
 
-	ThreadBucketPtr get(JNIEnv *jni_env) {
-		ThreadBucketPtr info((ThreadBucket*)map.get((map::KeyType)jni_env), false); // non-weak ref
-		if (info.defined())
-			GCHelper::signalSafepoint(info->localEpoch);
-		return info; // move
-	}
+  ThreadBucketPtr get(JNIEnv *jni_env) {
+    ThreadBucketPtr info((ThreadBucket *)map.get((map::KeyType)jni_env),
+                         false); // non-weak ref
+    if (info.defined())
+      GCHelper::signalSafepoint(info->localEpoch);
+    return info; // move
+  }
 
-	void remove(JNIEnv *jni_env) {
-		ThreadBucketPtr info((ThreadBucket*)map.remove((map::KeyType)jni_env)); // weak ref to object
-		if (info.defined())
-			GCHelper::detach(info->localEpoch);
-	}
+  void remove(JNIEnv *jni_env) {
+    ThreadBucketPtr info((ThreadBucket *)map.remove(
+        (map::KeyType)jni_env)); // weak ref to object
+    if (info.defined())
+      GCHelper::detach(info->localEpoch);
+  }
 };
 
-typedef ThreadMapBase<map::ConcurrentMapProvider<PointerHasher<JNIEnv>, true> > ThreadMap;
+typedef ThreadMapBase<map::ConcurrentMapProvider<PointerHasher<JNIEnv>, true>>
+    ThreadMap;
 
 #endif
--- src/test/cpp/test_thread_map.cpp
+++ src/test/cpp/test_thread_map.cpp
@@ -4,103 +4,155 @@
 
 #include "../../main/cpp/thread_map.h"
 
+#include <memory>
 #include <thread>
 #include <vector>
-#include <memory>
 
-#define PTR std::unique_ptr<JNIEnv>((JNIEnv*)new int(0))
+#define ptr() std::unique_ptr<JNIEnv>((JNIEnv *)new int(0))
 
 TEST(ThreadMapPutGetDeleteTest) {
-    ThreadMap map;
-    auto p1 = PTR;
-    auto pid = 999;
-    auto name = "name321";
+  ThreadMap map;
+  auto p1 = ptr();
+  auto pid = 999;
+  auto name = "name321";
 
-    // map is empty
-    CHECK(!map.get(p1.get()).defined());
+  // map is empty
+  CHECK(!map.get(p1.get()).defined());
 
-    map.put(p1.get(), name, pid);
+  map.put(p1.get(), name, pid);
 
-    // try to create temporary references to the bucket
-    for (int i = 0; i < 4; i++) {
-        ThreadBucketPtr r1 = map.get(p1.get());
-        CHECK(r1.defined());
-        CHECK_EQUAL(r1->tid, pid);
-        CHECK_EQUAL(r1->name, name);
-    }
+  // try to create temporary references to the bucket
+  for (int i = 0; i < 4; i++) {
+    ThreadBucketPtr r1 = map.get(p1.get());
+    CHECK(r1.defined());
+    CHECK_EQUAL(pid, r1->tid);
+    CHECK_EQUAL(name, r1->name);
+  }
 
-    map.remove(p1.get());
+  map.remove(p1.get());
 
-    // map is empty again
-    CHECK(!map.get(p1.get()).defined());
+  // map is empty again
+  CHECK(!map.get(p1.get()).defined());
 }
 
 TEST(ThreadMapPutDeleteGetTest) {
-    ThreadMap map;
-    auto p1 = PTR;
-    auto pid = 999;
-    auto name = "name321";
+  ThreadMap map;
+  auto p1 = ptr();
+  auto pid = 999;
+  auto name = "name321";
 
-    // map is empty
+  // map is empty
+  CHECK(!map.get(p1.get()).defined());
+
+  map.put(p1.get(), name, pid);
+
+  {
+    ThreadBucketPtr r1 = map.get(p1.get()), r2 = map.get(p1.get()),
+                    r3 = map.get(p1.get());
+    map.remove(p1.get());
     CHECK(!map.get(p1.get()).defined());
 
-    map.put(p1.get(), name, pid);
+    CHECK(r1.defined());
+    CHECK(r2.defined());
+    CHECK(r3.defined());
 
-    {
-        ThreadBucketPtr r1 = map.get(p1.get()), r2 = map.get(p1.get()), r3 = map.get(p1.get());
-        map.remove(p1.get());
-        CHECK(!map.get(p1.get()).defined());
+    CHECK_EQUAL(pid, r2->tid);
+    CHECK_EQUAL(name, r2->name);
+  }
 
-        CHECK(r1.defined());
-        CHECK(r2.defined());
-        CHECK(r3.defined());
-
-        CHECK_EQUAL(r2->tid, pid);
-        CHECK_EQUAL(r2->name, name);
-    }
-
-    CHECK(!map.get(p1.get()).defined());
+  CHECK(!map.get(p1.get()).defined());
 }
 
 TEST(ThreadMapAssignResetTest) {
-    ThreadMap map;
-    auto p1 = PTR, p2 = PTR;
-    auto pid1 = 999, pid2 = 111;
-    auto name1 = "name321", name2 = "name123";
+  ThreadMap map;
+  auto p1 = ptr(), p2 = ptr();
+  auto pid1 = 999, pid2 = 111;
+  auto name1 = "name321", name2 = "name123";
 
-    // map is empty
+  // map is empty
+  CHECK(!map.get(p1.get()).defined());
+  CHECK(!map.get(p2.get()).defined());
+
+  map.put(p1.get(), name1, pid1);
+  map.put(p2.get(), name2, pid2);
+
+  {
+    ThreadBucketPtr r1 = map.get(p1.get());
+    CHECK(r1.defined());
+    CHECK_EQUAL(pid1, r1->tid);
+    CHECK_EQUAL(name1, r1->name);
+
+    map.remove(p1.get());
     CHECK(!map.get(p1.get()).defined());
+
+    r1 = map.get(p2.get());
+    CHECK(r1.defined());
+    CHECK_EQUAL(pid2, r1->tid);
+    CHECK_EQUAL(name2, r1->name);
+
+    map.remove(p2.get());
     CHECK(!map.get(p2.get()).defined());
 
-    map.put(p1.get(), name1, pid1);
-    map.put(p2.get(), name2, pid2);
+    r1.reset();
+    CHECK(!r1.defined());
 
-    {
-        ThreadBucketPtr r1 = map.get(p1.get());
-        CHECK(r1.defined());
-        CHECK_EQUAL(r1->tid, pid1);
-        CHECK_EQUAL(r1->name, name1);
+    r1.reset();
+  }
 
-        map.remove(p1.get());
-        CHECK(!map.get(p1.get()).defined());
+  // map is empty
+  CHECK(!map.get(p1.get()).defined());
+  CHECK(!map.get(p2.get()).defined());
+}
 
-        r1 = map.get(p2.get());
-        CHECK(r1.defined());
-        CHECK_EQUAL(r1->tid, pid2);
-        CHECK_EQUAL(r1->name, name2);
+void reader(ThreadMap &map, const int id, JNIEnv *const p,
+            std::atomic_int &read) {
+  ThreadBucketPtr ptr = map.get(p);
+  std::string name("thread-" + std::to_string(id));
 
-        map.remove(p2.get());
-        CHECK(!map.get(p2.get()).defined());
+  while (!ptr.defined()) {
+    sched_yield();
+    ptr = map.get(p);
+  }
 
-        r1.reset();
-        CHECK(!r1.defined());
+  read.fetch_add(1, std::memory_order_seq_cst);
+  do {
+    CHECK_EQUAL(name, ptr->name);
+    CHECK_EQUAL(id, ptr->tid);
+  } while ((ptr = map.get(p)).defined());
+}
 
-        r1.reset();
-    }
+TEST(ThreadMapConcurrentTest) {
+  const int sz = 4;
+  ThreadMap map;
 
-    // map is empty
-    CHECK(!map.get(p1.get()).defined());
-    CHECK(!map.get(p2.get()).defined());
+  std::atomic_int read(0);
+  std::vector<std::unique_ptr<JNIEnv>> ps(sz);
+  std::vector<std::thread> tvec(sz);
+
+  for (auto it = ps.begin(); it != ps.end(); ++it) {
+    *it = ptr();
+    int id = it - ps.begin();
+    tvec[id] =
+        std::thread(reader, std::ref(map), id, it->get(), std::ref(read));
+  }
+
+  for (auto it = ps.begin(); it != ps.end(); ++it) {
+    int id = it - ps.begin();
+    std::string name("thread-" + std::to_string(id));
+    map.put(it->get(), name.c_str(), id);
+  }
+
+  while (read.load(std::memory_order_acquire) != sz)
+    sched_yield();
+
+  for (auto it = ps.begin(); it != ps.end(); ++it) {
+    map.remove(it->get());
+  }
+
+  for (auto it = tvec.begin(); it != tvec.end(); ++it) {
+    it->join();
+    CHECK(!map.get(ps[it - tvec.begin()].get()).defined());
+  }
 }
 
 #endif // DISABLE_CPP11
